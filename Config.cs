b672b50c7fed6271d138a7ba635ac0fefdc876cc
RyoTune <zutomayofan50@gmail.com>
2025-06-13 20:50:58 -0500

    Add 1.0.3b code.

--- Config.cs
+++ Config.cs
@@ -1,5 +1,6 @@
 ﻿using P5R.BatonPassRecovery.Template.Configuration;
 using System.ComponentModel;
+using Reloaded.Mod.Interfaces.Structs;
 
 namespace P5R.BatonPassRecovery.Configuration;
 
@@ -8,6 +9,66 @@ public class Config : Configurable<Config>
     [DisplayName("Log Level")]
     [DefaultValue(LogLevel.Information)]
     public LogLevel LogLevel { get; set; } = LogLevel.Information;
+    
+    [Category("Baton Pass HP Recovery %")]
+    [DisplayName("Rank 1")]
+    [DefaultValue(0)]
+    [SliderControlParams(
+        minimum: 0.0,
+        maximum: 100.0,
+        smallChange: 1.0,
+        largeChange: 10.0,
+        tickFrequency: 10,
+        isSnapToTickEnabled: false,
+        tickPlacement: SliderControlTickPlacement.BottomRight,
+        showTextField: true,
+        isTextFieldEditable: false)]
+    public int HP_Level_1 { get; set; } = 0;
+
+    [Category("Baton Pass HP Recovery %")]
+    [DisplayName("Rank 2")]
+    [DefaultValue(10)]
+    [SliderControlParams(
+        minimum: 0.0,
+        maximum: 100.0,
+        smallChange: 1.0,
+        largeChange: 10.0,
+        tickFrequency: 10,
+        isSnapToTickEnabled: false,
+        tickPlacement: SliderControlTickPlacement.BottomRight,
+        showTextField: true,
+        isTextFieldEditable: false)]
+    public int HP_Level_2 { get; set; } = 10;
+
+    [Category("Baton Pass HP Recovery %")]
+    [DisplayName("Rank 3")]
+    [DefaultValue(15)]
+    [SliderControlParams(
+        minimum: 0.0,
+        maximum: 100.0,
+        smallChange: 1.0,
+        largeChange: 10.0,
+        tickFrequency: 10,
+        isSnapToTickEnabled: false,
+        tickPlacement: SliderControlTickPlacement.BottomRight,
+        showTextField: true,
+        isTextFieldEditable: false)]
+    public int HP_Level_3 { get; set; } = 15;
+
+    [Category("Baton Pass SP Recovery")]
+    [DisplayName("Turn 1")]
+    [DefaultValue(2)]
+    public int SP_Level_1 { get; set; } = 2;
+
+    [Category("Baton Pass SP Recovery")]
+    [DisplayName("Turn 2")]
+    [DefaultValue(4)]
+    public int SP_Level_2 { get; set; } = 4;
+
+    [Category("Baton Pass SP Recovery")]
+    [DisplayName("Turn 3")]
+    [DefaultValue(6)]
+    public int SP_Level_3 { get; set; } = 6;
 }
 
 /// <summary>
--- Mod.cs
+++ Mod.cs
@@ -1,14 +1,21 @@
 ﻿#if DEBUG
 using System.Diagnostics;
 #endif
-using Reloaded.Hooks.ReloadedII.Interfaces;
+using System.Runtime.InteropServices;
+using System.Text.Json;
 using Reloaded.Mod.Interfaces;
 using P5R.BatonPassRecovery.Template;
 using P5R.BatonPassRecovery.Configuration;
+using Reloaded.Hooks.Definitions;
+using Reloaded.Hooks.Definitions.Enums;
+using Reloaded.Hooks.Definitions.X64;
+using Reloaded.Mod.Interfaces.Internal;
+using static Reloaded.Hooks.Definitions.X64.FunctionAttribute;
+using IReloadedHooks = Reloaded.Hooks.ReloadedII.Interfaces.IReloadedHooks;
 
 namespace P5R.BatonPassRecovery;
 
-public class Mod : ModBase
+public unsafe class Mod : ModBase
 {
     private readonly IModLoader _modLoader;
     private readonly IReloadedHooks? _hooks;
@@ -17,6 +24,27 @@ public class Mod : ModBase
 
     private Config _config;
     private readonly IModConfig _modConfig;
+    
+    [Function(Register.rax, Register.rax, true)]
+    private delegate ushort GetBatonSpAdd(int batonPassLevel);
+    private IReverseWrapper<GetBatonSpAdd>? _batonSpWrapper;
+    private IAsmHook? _batonSpHook;
+
+    [Function(Register.rax, Register.rax, true)]
+    private delegate ushort GetBatonHpAdd(int batonPassLevel);
+    private IReverseWrapper<GetBatonSpAdd>? _batonHpWrapper;
+    private IAsmHook? _batonHpHook;
+
+    private IAsmHook? _removeHpBatonCheckHook;
+    private IAsmHook? _removeHpCheckHook;
+    private IAsmHook? _hpMathCheck;
+    private IAsmHook? _hpRecoverMath;
+
+    private int* _batonHpRecovery1;
+    private int* _batonHpRecovery2;
+    private int* _batonHpRecovery3;
+
+    private bool _configLoaded;
 
     public Mod(ModContext context)
     {
@@ -31,6 +59,142 @@ public class Mod : ModBase
 #endif
         Project.Initialize(_modConfig, _modLoader, _log, true);
         Log.LogLevel = _config.LogLevel;
+        
+        ScanHooks.Add("Baton Pass SP Hook", "44 8B C8 85 C0 74", (hooks, result) =>
+        {
+            
+            var patch = new string[]
+            {
+                "use64",
+                Utilities.PushCallerRegisters,
+                hooks.Utilities.GetAbsoluteCallMnemonics(GetBatonPassSpAddImpl, out _batonSpWrapper),
+                Utilities.PopCallerRegisters,
+                "add r15d, eax",
+                hooks.Utilities.GetAbsoluteJumpMnemonics(result + 0xD2, true)
+            };
+
+            _batonSpHook = hooks.CreateAsmHook(patch, result).Activate();
+        });
+
+        _batonHpRecovery1 = (int*)Marshal.AllocHGlobal(sizeof(int));
+        _batonHpRecovery2 = (int*)Marshal.AllocHGlobal(sizeof(int));
+        _batonHpRecovery3 = (int*)Marshal.AllocHGlobal(sizeof(int));
+        ApplyHpSettings();
+
+        ScanHooks.Add("Baton Pass HP Hooks", "0F 84 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8B 4D ?? 48 8B 49", (hooks, result) =>
+        {
+            var removeHpBatonCheckAddress = result - 0x31;
+            string[] removeHpBatonCheckPatch =
+            [
+                "use64",
+                "mov r15d, ebx"
+            ];
+
+            _removeHpBatonCheckHook = hooks.CreateAsmHook(removeHpBatonCheckPatch, removeHpBatonCheckAddress, AsmHookBehaviour.DoNotExecuteOriginal).Activate();
+
+            var removeHpBatonCheck = new string[] { "use64", "test rax, rax" };
+            _removeHpCheckHook = hooks.CreateAsmHook(removeHpBatonCheck, result).Activate();
+
+            var hpMathCheckAddress = result + 0x18;
+            var hpMathCheckPatch = new string[]
+            {
+                "use64",
+                $"mov rcx, {(nint)_batonHpRecovery1}",
+                "cmp r15d, 0",
+                "je getRecoverValue",
+                "cmp r15d, 1",
+                $"mov rcx, {(nint)_batonHpRecovery2}",
+                "je getRecoverValue",
+                $"mov rcx, {(nint)_batonHpRecovery3}",
+                "getRecoverValue:",
+                "mov ecx, [rcx]",
+                "imul ecx, eax",
+                "mov eax, 0x51eb851f"
+            };
+
+            _hpMathCheck = hooks.CreateAsmHook(hpMathCheckPatch, hpMathCheckAddress, AsmHookBehaviour.DoNotExecuteOriginal).Activate();
+
+            var hpRecoverMathAddress = result + 0x6C;
+            var hpMRecoverMathPatch = new string[]
+            {
+                "use64",
+                $"mov rcx, {(nint)_batonHpRecovery1}",
+                "cmp r15d, 0",
+                "je getRecoverValue",
+                "cmp r15d, 1",
+                $"mov rcx, {(nint)_batonHpRecovery2}",
+                "je getRecoverValue",
+                $"mov rcx, {(nint)_batonHpRecovery3}",
+                "getRecoverValue:",
+                "mov ecx, [rcx]",
+                "imul ecx, eax",
+                "mov esi, 0x8",
+            };
+
+            _hpRecoverMath = hooks.CreateAsmHook(hpMRecoverMathPatch, hpRecoverMathAddress, AsmHookBehaviour.DoNotExecuteOriginal).Activate();
+        });
+
+        _modLoader.ModLoaded += OnModLoaded;
+    }
+
+    private void OnModLoaded(IModV1 mod, IModConfigV1 modConfig)
+    {
+        var modDir = _modLoader.GetDirectoryForModId(modConfig.ModId);
+        var batonConfigFile = Path.Join(modDir, "baton-pass.json");
+        if (!_configLoaded && File.Exists(batonConfigFile))
+        {
+            try
+            {
+                var config = JsonSerializer.Deserialize<Config>(File.ReadAllText(batonConfigFile));
+                if (config == null)
+                {
+                    return;
+                }
+
+                _config.HP_Level_1 = _config.HP_Level_1;
+                _config.HP_Level_2 = _config.HP_Level_2;
+                _config.HP_Level_3 = _config.HP_Level_3;
+                _config.SP_Level_1 = _config.SP_Level_1;
+                _config.SP_Level_2 = _config.SP_Level_2;
+                _config.SP_Level_3 = _config.SP_Level_3;
+                ApplyHpSettings();
+
+                _configLoaded = true;
+                Log.Information($"Using mod baton config: {modConfig.ModName}");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, $"Failed to load baton config from: {modConfig.ModName}");
+            }
+        }
+    }
+
+    private ushort GetBatonPassSpAddImpl(int batonPassLevel)
+    {
+        ushort spIncrease = 0;
+        switch (batonPassLevel)
+        {
+            case 0:
+                spIncrease = (ushort)_config.SP_Level_1;
+                break;
+            case 1:
+                spIncrease = (ushort)_config.SP_Level_2;
+                break;
+            case 2:
+                spIncrease = (ushort)_config.SP_Level_3;
+                break;
+        }
+
+        Log.Debug($"Baton Pass LV: {batonPassLevel + 1}");
+        Log.Debug($"SP: +{spIncrease}");
+        return spIncrease;
+    }
+
+    private void ApplyHpSettings()
+    {
+        *_batonHpRecovery1 = _config.HP_Level_1;
+        *_batonHpRecovery2 = _config.HP_Level_2;
+        *_batonHpRecovery3 = _config.HP_Level_3;
     }
 
     #region Standard Overrides
@@ -42,6 +206,7 @@ public class Mod : ModBase
         _config = configuration;
         _log.WriteLine($"[{_modConfig.ModId}] Config Updated: Applying");
         Log.LogLevel = _config.LogLevel;
+        ApplyHpSettings();
     }
 
     #endregion
